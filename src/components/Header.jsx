dbda828a7c3ee4f6f08d772e77ac857a997b6569
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-03-14 23:39:18 +0300

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <div className="header-container">
+            <a href="#" className="logo">navity</a>
+            <div className="nav-links">
+                <a href="">About</a>
+                <a href="">Vans</a>
+                <a href="">Vendor</a>
+                <a href="">Login</a>
+            </div>
+        </div>
+    )
+}
