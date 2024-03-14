f04c3912b5d546c68aa602b79a0d3ed5f306b8f3
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-03-14 23:38:50 +0300

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Home() {
+    return (
+        <div className="home-page">
+            <div className="home-page-container">
+                <h1>You got the travel plans, we got the travel vans.</h1>
+                <p>Add adventure to your life by joining the #navity movement. Rent the perfect van to make your perfect road trip.</p>
+                <a href="#">Find your van</a>
+            </div>
+        </div>
+    )
+}
