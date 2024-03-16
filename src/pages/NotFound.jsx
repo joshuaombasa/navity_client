9dad3000d03edee56a55f73574c408637e4500c8
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-03-16 23:52:54 +0300

    initial commit

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function NotFound() {
+    return (
+        <div className="notfound">
+            <div className="notfound-container">
+                <h1>Sorry, the page you were looking for was not found.</h1>
+                <a href="#">Return to home</a>
+            </div>
+        </div>
+    )
+}
