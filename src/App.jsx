b27771c6d2cf7e182de014edb64281e42834fb88
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-03-16 23:52:43 +0300

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import { useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import './App.css'
 import Layout from './components/Layout'
+import About from './pages/About'
+import Home from './pages/Home'
 
 function App() {
 
 
   return (
     <>
-      <Layout/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path='about' element={<About />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
