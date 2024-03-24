36f6e953ac70190a7070e38c4f93aa77b398b061
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-03-25 02:25:01 +0300

    initial commit

---
+++ src/components/SelectedHostVanPhotos.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function SelectedHostVanPhotos() {
+    return (
+        <div className="selected-host-photos">
+            <h1>selected host van Photos</h1>
+        </div>
+    )
+}
