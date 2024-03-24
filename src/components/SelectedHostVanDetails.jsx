89ee175dab14192a7ae44b2c26ba34e2b6ded1af
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-03-25 02:24:50 +0300

    initial commit

---
+++ src/components/SelectedHostVanDetails.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function SelectedHostVanDetails() {
+    return (
+        <div className="selected-host-details">
+            <h1>selected host van details</h1>
+        </div>
+    )
+}
