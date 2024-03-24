6b63ba2cf80171e258ccca1a92f0eec65d503fd7
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-03-25 02:25:53 +0300

    initial commit

---
+++ src/pages/host/HostVans.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import aboutImage from "../../../public/about--page.png"
+import { Link } from "react-router-dom";
+
+export default function HostVans() {
+    const vanOption = { id: "6", name: "Green Wonder", price: 70, description: "With this van, you can take your travel life to the next level. The Green Wonder is a sustainable vehicle that's perfect for people who are looking for a stylish, eco-friendly mode of transport that can go anywhere.", imageUrl: "https://assets.scrimba.com/advanced-react/react-router/green-wonder.png", type: "rugged" }
+
+
+    return (
+        <div className="">
+            <h1>Host Vans</h1>
+            <Link to={vanOption.id} className="van-item">
+                <img src={aboutImage} alt="" />
+                <div className="host-van-item-info">
+                    <h3>{vanOption.name}</h3>
+                    <h4>${vanOption.price}/day</h4>
+                </div>
+            </Link>
+            <Link to={vanOption.id} className="van-item">
+                <img src={aboutImage} alt="" />
+                <div className="host-van-item-info">
+                    <h3>{vanOption.name}</h3>
+                    <h4>${vanOption.price}/day</h4>
+                </div>
+            </Link>
+            <Link to={vanOption.id} className="van-item">
+                <img src={aboutImage} alt="" />
+                <div className="host-van-item-info">
+                    <h3>{vanOption.name}</h3>
+                    <h4>${vanOption.price}/day</h4>
+                </div>
+            </Link>
+        </div>
+    )
+}
