53760f209c4f4cef6498a3403195ffc665cdc89d
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-03-24 23:58:18 +0300

    initial commit

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 export default function NotFound() {
     return (
         <div className="notfound">
             <div className="notfound-container">
                 <h1>Sorry, the page you were looking for was not found.</h1>
-                <a href="#">Return to home</a>
+                <Link to='/'>Return to home</Link>
             </div>
         </div>
     )
