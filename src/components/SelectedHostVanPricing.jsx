4b71622ab2c25531fc0d3b4be7d0250220178050
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-03-25 02:25:43 +0300

    initial commit

---
+++ src/components/SelectedHostVanPricing.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function SelectedHostVanPricing() {
+    return (
+        <div className="selected-host-pricing">
+            <h1>selected host van Pricing</h1>
+        </div>
+    )
+}
