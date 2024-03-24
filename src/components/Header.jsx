a21e3f0f666cc23ceb0aa0b36fad4371c04afc58
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-03-25 02:24:24 +0300

    initial commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
             <div className="nav-links">
                 <NavLink to="about">About</NavLink>
                 <NavLink to="vans">Vans</NavLink>
-                <NavLink to="vendor">Vendor</NavLink>
+                <NavLink to="host">Host</NavLink>
                 <NavLink to="login">Login</NavLink>
             </div>
         </div>
