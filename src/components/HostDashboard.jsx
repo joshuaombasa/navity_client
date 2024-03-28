73da42e4b6d73b28203f78cc1f1a5377f52bf6d2
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-03-28 23:54:33 +0300

    initial commit

--- src/components/HostDashboard.jsx
+++ src/components/HostDashboard.jsx
@@ -3,14 +3,16 @@ import { NavLink, Outlet } from "react-router-dom";
 
 export default function HostDashboard() {
     return (
-        <div className="">
-            <nav>
-                <NavLink to="." end>Dashboard</NavLink>
-                <NavLink to="income">Income</NavLink>
-                <NavLink to="vans">Vans</NavLink>
-                <NavLink to="reviews">Reviews</NavLink>
-            </nav>
-            <Outlet/>
+        <div className="host-dashboard">
+            <div className="host-dashboard-container">
+                <nav>
+                    <NavLink to="." end>Dashboard</NavLink>
+                    <NavLink to="income">Income</NavLink>
+                    <NavLink to="vans">Vans</NavLink>
+                    <NavLink to="reviews">Reviews</NavLink>
+                </nav>
+                <Outlet />
+            </div>
         </div>
     )
 }
