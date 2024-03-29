e72e03edd6131fc2df7c09c21bed4a3baeeee8e8
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-03-29 16:57:47 +0300

    initial commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 export default function Login() {
 
     const [formData, setFormData] = useState({
-        email:'',
+        email: '',
         password: ''
     })
 
     function handleChange(event) {
-        const {name,value} = event.target
+        const { name, value } = event.target
         setFormData(prevState => ({
             ...prevState,
             [name]: value
@@ -21,22 +21,29 @@ export default function Login() {
     }
     return (
         <div className="login-page">
-            <div className="container">
+            <div className="login-page-container">
                 <h1>Sign in to your account</h1>
-                <form onSubmit={handleSubmit}>
-                    <input 
-                         type="text" 
-                         name="email"
-                         value={FormData.email}
-                         onChange={handleChange}
-                    />
-                     <input 
-                         type="text" 
-                         name="password"
-                         value={FormData.password}
-                         onChange={handleChange}
-                    />
+                <form onSubmit={handleSubmit} className="login-form">
+                    <div className="login-inputs">
+                        <input
+                            type="text"
+                            name="email"
+                            value={FormData.email}
+                            onChange={handleChange}
+                            placeholder="Email address"
+                            className="email"
+                        />
+                        <input
+                            type="text"
+                            name="password"
+                            value={FormData.password}
+                            onChange={handleChange}
+                            placeholder="Password"
+                            className="password"
+                        />
+                    </div>
                     <button>Sign in</button>
+                    <span>Don’t have an account? <Link className="create-account-link">Create one now</Link></span>
                 </form>
             </div>
         </div>
